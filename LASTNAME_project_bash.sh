58258530d445ca23387e5a8db5e805702ede6d9b
Laura Sisk-Hackworth <56939474+laurasisk@users.noreply.github.com>
2024-02-08 17:06:30 -0800

    Add files via upload

---
+++ LASTNAME_project_bash.sh
@@ -0,0 +1,26 @@
+
+### Make this a bash script that can be executed by putting
+### a "shebang" at the top of the file
+
+echo "It works!"
+
+
+### (1) In your home directory make a directory called RAW_DATA
+
+### (2) Copy all .fna fasta files  
+###     from home directory into RAW_DATA (must work from any directory.)
+
+### (3) Do the same with all primer files ending with .csv
+
+### (4) In your home directory, make 2 directories: P_DATA and RESULTS
+
+### (5) Add all three directories to your $PATH: ~/RAW_DATA, ~/P_DATA
+###     and ~/RESULTS
+
+### (6) Write your entire $PATH, the text string RAW_DATA and the 
+###    contents of this directory (the names of the files) into a new file 
+###    called 'readme.txt' in your home directory
+
+### (7) The last command should output the contents of readme.txt
+###     to the terminal.
+
